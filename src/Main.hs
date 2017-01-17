f775eafff2e1ffacbc9d159916abe7e011c3a07d
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-01-17 22:13:52 +0000

    initial source

---
+++ src/Main.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Applicative
+import Options.Applicative
+import System.Exit
+import Text.Comma
+import Text.Tabl
+import qualified Data.Attoparsec.Text as A
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+import Options
+
+-- | Parse the visual decoration settings.
+parseDecor :: A.Parser Decoration -- ^ parser
+parseDecor = A.string "all"     *> pure DecorAll
+         <|> A.string "none"    *> pure DecorNone
+         <|> A.string "inner"   *> pure DecorInner
+         <|> A.string "outer"   *> pure DecorOuter
+         <|> A.string "only("   *> (DecorOnly   <$> decimal) <* A.char ')'
+         <|> A.string "except(" *> (DecorExcept <$> decimal) <* A.char ')'
+         <|> A.string "union("  *> (DecorUnion  <$> recurse) <* A.char ')'
+         <|> A.string "isect("  *> (DecorIsect  <$> recurse) <* A.char ')'
+  where
+    recurse = A.sepBy parseDecor (A.char ',')
+    decimal = A.sepBy A.decimal  (A.char ',')
+
+-- | Parse the column alignment settings.
+parseAligns :: A.Parser [Alignment] -- ^ parser
+parseAligns = A.sepBy align (A.char ',')
+  where
+    align = A.string "left"   *> pure AlignLeft
+        <|> A.string "centre" *> pure AlignCentre
+        <|> A.string "right"  *> pure AlignRight
+
+-- | Create a table based on the file content and command-line options.
+tablize :: Options              -- ^ command-line options
+        -> [[T.Text]]           -- ^ CSV table
+        -> Either String T.Text -- ^ error | table
+tablize options cells = do
+  hdecor <- A.parseOnly parseDecor  (optHorizontal options)
+  vdecor <- A.parseOnly parseDecor  (optVertical options)
+  aligns <- A.parseOnly parseAligns (optAlignment options)
+  return $ tabl EnvAscii hdecor vdecor aligns cells
+
+-- | Perform the final output of the program.
+result :: Either String T.Text -- ^ error | table
+       -> IO ()                -- ^ action
+result = either failure success
+  where
+    failure err = T.putStrLn ("ERROR: " <> T.pack err) >> exitFailure
+    success res = T.putStrLn res                       >> exitSuccess
+
+-- | Read the input file text. In case that no file was specified, the 
+-- standard input is used.
+getInput :: Options   -- ^ command-line options
+         -> IO T.Text -- ^ input
+getInput options = case optFile options of
+  Just file -> T.readFile file
+  Nothing   -> T.getContents
+
+-- | Pretty-printing of CSV files.
+main :: IO ()
+main = do
+  options <- execParser Options.parser
+  input   <- getInput options
+  case comma input of
+    Left err  -> T.putStrLn ("ERROR: " <> T.pack err) >> exitFailure
+    Right csv -> result (tablize options csv)
---
+++ src/Options.hs
@@ -0,0 +1,67 @@
+module Options
+( Options(..)
+, parser
+) where
+
+import Data.Char
+import Options.Applicative
+import qualified Data.Text as T
+
+-- | Command-line options.
+data Options = Options
+  { optFile       :: Maybe String
+  , optHorizontal :: T.Text 
+  , optVertical   :: T.Text 
+  , optAlignment  :: T.Text }
+
+-- | Relevant file extensions options.
+parseFile :: Parser (Maybe String) -- ^ parser
+parseFile = optional $ strArgument (metavar "FILE")
+
+-- | Helper funciton to remove all whitespace from a string.
+noSpace :: String -- ^ old string
+        -> String -- ^ new string
+noSpace = filter (not . isSpace)
+
+-- | Parse horizontal decoration.
+parseHorizontal :: Parser T.Text -- ^ parser
+parseHorizontal = fmap (T.pack . noSpace) $ strOption
+   $ short   'x'
+  <> long    "horizontal"
+  <> value   "union(only(1),outer)"
+  <> metavar "HDECOR"
+  <> help    "Horizontal decoration"
+  <> showDefault
+
+-- | Relevant file extensions options.
+parseVertical :: Parser T.Text -- ^ parser
+parseVertical = fmap (T.pack . noSpace) $ strOption
+   $ short   'y'
+  <> long    "vertical"
+  <> value   "all"
+  <> metavar "VDECOR"
+  <> help    "Vertical decoration"
+  <> showDefault
+
+-- | Relevant file extensions options.
+parseAlignment :: Parser T.Text -- ^ parser
+parseAlignment = fmap (T.pack . noSpace) $ strOption
+   $ short   'a'
+  <> long    "alignment"
+  <> value   ""
+  <> metavar "ALIGNS"
+  <> help    "Comma-separated list of column alignments. E.g.: left,right"
+  <> showDefault
+
+-- | Command-line user interface.
+optionsParser :: Parser Options -- ^ parser
+optionsParser = Options
+  <$> parseFile
+  <*> parseHorizontal
+  <*> parseVertical 
+  <*> parseAlignment 
+
+-- | Parser of the command-line options.
+parser :: ParserInfo Options -- ^ parser
+parser = info (helper <*> optionsParser) (header top <> fullDesc)
+  where top = "tablize - CSV file pretty-printer"
