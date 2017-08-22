3a49969825dcbecf9ccf8b55db896c628866c7f3
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-08-22 22:41:42 +0100

    alternative sum

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Applicative
+import Data.Foldable
 import Options.Applicative
 import System.Exit
 import Text.Comma
@@ -14,15 +15,16 @@ import Options
 
 -- | Parse the visual decoration settings.
 parseDecor :: A.Parser Decoration -- ^ parser
-parseDecor = A.string "all"     *> pure DecorAll
-         <|> A.string "none"    *> pure DecorNone
-         <|> A.string "inner"   *> pure DecorInner
-         <|> A.string "outer"   *> pure DecorOuter
-         <|> A.string "only("   *> (DecorOnly   <$> decimal) <* A.char ')'
-         <|> A.string "except(" *> (DecorExcept <$> decimal) <* A.char ')'
-         <|> A.string "union("  *> (DecorUnion  <$> recurse) <* A.char ')'
-         <|> A.string "isect("  *> (DecorIsect  <$> recurse) <* A.char ')'
-         A.<?> "invalid decoration"
+parseDecor = asum
+  [ A.string "all"     *> pure DecorAll
+  , A.string "none"    *> pure DecorNone
+  , A.string "inner"   *> pure DecorInner
+  , A.string "outer"   *> pure DecorOuter
+  , A.string "only("   *> (DecorOnly   <$> decimal) <* A.char ')'
+  , A.string "except(" *> (DecorExcept <$> decimal) <* A.char ')'
+  , A.string "union("  *> (DecorUnion  <$> recurse) <* A.char ')'
+  , A.string "isect("  *> (DecorIsect  <$> recurse) <* A.char ')'
+  ] A.<?> "invalid decoration"
   where
     recurse = A.sepBy parseDecor (A.char ',')
     decimal = A.sepBy A.decimal  (A.char ',')
@@ -31,10 +33,11 @@ parseDecor = A.string "all"     *> pure DecorAll
 parseAligns :: A.Parser [Alignment] -- ^ parser
 parseAligns = (A.endOfInput *> pure []) <|> A.sepBy1 align (A.char ',')
   where
-    align = A.string "left"   *> pure AlignLeft
-        <|> A.string "centre" *> pure AlignCentre
-        <|> A.string "right"  *> pure AlignRight
-        A.<?> "invalid alignment"
+    align = asum
+      [ A.string "left"   *> pure AlignLeft
+      , A.string "centre" *> pure AlignCentre
+      , A.string "right"  *> pure AlignRight
+      ] A.<?> "invalid alignment"
 
 -- | Create a table based on the file content and command-line options.
 tablize :: Options              -- ^ command-line options
